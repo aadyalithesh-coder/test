05f6565c562eb031f18f8a31b19d2e318c8836f8
coder77182 <aadyalithesh@gmail.com>
2026-06-19 18:03:19 +0200

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,8 @@
+student1={"name":"steve","score":"90"}
+student2={"name":"kevin","score":"5"}
+student3={"name":"emily","score":"82"}
+student4={"name":"bob","score":"25"}
+student5={"name":"ava","score":"50"}
+
+average=(student1["score"]+student2["score"]+student3["score"]+student4["score"]+student5["score"]/2)
+print(average)
